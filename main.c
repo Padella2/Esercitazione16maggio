870b8e39aa3df091446041661b4cea3731af66db
Nicola <nicola.scardoni@salesianiverona.it>
2023-05-16 09:39:33 +0200

    Controllo numeri negativi, main.c

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 
 int main(void){
 	int v[10] = {0};
-	int i = 0, max = -99999;
+	int i = 0, max = -1;
 	
 	for(i = 0; i < 10; i++){
-		printf("Inserisci il numero: ");
-		scanf("%d", &v[i]);
+		do{
+			printf("Inserisci il numero: ");
+			scanf("%d", &v[i]);
+			if(v[i] < 0){
+				printf("Errore\n");
+			}
+		}while(v[i] < 0);
 	}
 	
 	for(i = 0; i < 10; i++){
